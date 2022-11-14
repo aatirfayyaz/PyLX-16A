d2fe339c8ca599066680fb633caebc6b8bcaa0c7
Aatir Fayyaz <aatir.fayyaz@columbia.edu>
2022-11-14 17:10:50 -0500

    Troubleshooting on physical robot

--- walk.py
+++ walk.py
@@ -103,6 +103,8 @@ servo2Final = servo2Home + angleChange
 servo3Final = servo3Home - angleChange
 servo4Final = servo4Home + angleChange
 
+steps = 10
+
 t = 0
 while t < 3:
     servo1.move(-(0.7407 * (t ** 5)) + (5.556 * (t ** 4)) - (11.111 * (t ** 3)) + servo1Home)  # forward move
@@ -110,20 +112,21 @@ while t < 3:
     time.sleep(0.05)
     t += 0.1
 
-t = 0
-while t < 3:
-    servo2.move((0.7407 * (t ** 5)) - (5.556 * (t ** 4)) + (11.111 * (t ** 3)) + servo2Home)  # forward move
-    servo3.move(-(0.7407 * (t ** 5)) + (5.556 * (t ** 4)) - (11.111 * (t ** 3)) + servo3Home)  # forward move
-    servo1.move((0.7407 * (t ** 5)) - (5.556 * (t ** 4)) + (11.111 * (t ** 3)) + servo1Final)  # backward move
-    servo4.move(-(0.7407 * (t ** 5)) + (5.556 * (t ** 4)) - (11.111 * (t ** 3)) + servo4Final)  # backward move
-    time.sleep(0.05)
-    t += 0.1
+for step in range(steps):
+    t = 0
+    while t < 3:
+        servo2.move((0.7407 * (t ** 5)) - (5.556 * (t ** 4)) + (11.111 * (t ** 3)) + servo2Home)  # forward move
+        servo3.move(-(0.7407 * (t ** 5)) + (5.556 * (t ** 4)) - (11.111 * (t ** 3)) + servo3Home)  # forward move
+        servo1.move((0.7407 * (t ** 5)) - (5.556 * (t ** 4)) + (11.111 * (t ** 3)) + servo1Final)  # backward move
+        servo4.move(-(0.7407 * (t ** 5)) + (5.556 * (t ** 4)) - (11.111 * (t ** 3)) + servo4Final)  # backward move
+        time.sleep(0.05)
+        t += 0.1
 
-t = 0
-while t < 3:
-    servo2.move(-(0.7407 * (t ** 5)) + (5.556 * (t ** 4)) - (11.111 * (t ** 3)) + servo2Final)  # backward move
-    servo3.move((0.7407 * (t ** 5)) - (5.556 * (t ** 4)) + (11.111 * (t ** 3)) + servo3Final)  # backward move
-    servo1.move(-(0.7407 * (t ** 5)) + (5.556 * (t ** 4)) - (11.111 * (t ** 3)) + servo1Home)  # forward move
-    servo4.move((0.7407 * (t ** 5)) - (5.556 * (t ** 4)) + (11.111 * (t ** 3)) + servo4Home)  # forward move
-    time.sleep(0.05)
-    t += 0.1
+    t = 0
+    while t < 3:
+        servo2.move(-(0.7407 * (t ** 5)) + (5.556 * (t ** 4)) - (11.111 * (t ** 3)) + servo2Final)  # backward move
+        servo3.move((0.7407 * (t ** 5)) - (5.556 * (t ** 4)) + (11.111 * (t ** 3)) + servo3Final)  # backward move
+        servo1.move(-(0.7407 * (t ** 5)) + (5.556 * (t ** 4)) - (11.111 * (t ** 3)) + servo1Home)  # forward move
+        servo4.move((0.7407 * (t ** 5)) - (5.556 * (t ** 4)) + (11.111 * (t ** 3)) + servo4Home)  # forward move
+        time.sleep(0.05)
+        t += 0.1
