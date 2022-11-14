112a06e3aa148cad969d9c713b9c99709a7a459d
Aatir Fayyaz <aatir.fayyaz@columbia.edu>
2022-11-14 13:58:26 -0500

    Added a walk.py file for my robot

---
+++ walk.py
@@ -0,0 +1,109 @@
+from math import sin, cos
+from lx16a import *
+import time
+
+LX16A.initialize("COM3", 0.1)
+
+try:
+    servo1 = LX16A(111)
+    servo2 = LX16A(121)
+    servo3 = LX16A(211)
+    servo4 = LX16A(221)
+
+    servo1.set_angle_limits(0, 240)
+    servo2.set_angle_limits(0, 240)
+    servo3.set_angle_limits(0, 240)
+    servo4.set_angle_limits(0, 240)
+
+except ServoTimeoutError as e:
+    print(f"Servo {e.id_} is not responding. Exiting...")
+    quit()
+
+# Homing / Boot sequence
+''' This is a homing/boot sequence to check servo motors are moving correctly.
+The sequence moves each servo by +5 and -10 degrees from current position and then
+homes it at 120 degrees. A time delay of 0.5s between moves has been implemented.'''
+
+time.sleep(2)
+
+servo1Home = 120
+servo2Home = 125
+servo3Home = 100
+servo4Home = 145
+
+try:
+    servo1.move(servo1Home + 10)
+    time.sleep(0.5)
+    servo1.move(servo1Home - 10)
+    time.sleep(0.5)
+    servo1.move(servo1Home)
+    time.sleep(0.5)
+
+except:
+    print("Servo 1 error during homing sequence")
+    quit()
+
+try:
+    servo2.move(servo2Home + 10)
+    time.sleep(0.5)
+    servo2.move(servo2Home - 10)
+    time.sleep(0.5)
+    servo2.move(servo2Home)
+    time.sleep(0.5)
+
+except:
+    print("Servo 2 error during homing sequence")
+    quit()
+
+try:
+    servo3.move(servo3Home + 10)
+    time.sleep(0.5)
+    servo3.move(servo3Home - 10)
+    time.sleep(0.5)
+    servo3.move(servo3Home)
+    time.sleep(0.5)
+
+except:
+    print("Servo 3 error during homing sequence")
+    quit()
+
+try:
+    servo4.move(servo4Home + 10)
+    time.sleep(0.5)
+    servo4.move(servo4Home - 10)
+    time.sleep(0.5)
+    servo4.move(servo4Home)
+    time.sleep(0.5)
+
+except:
+    print("Servo 4 error during homing sequence")
+    quit()
+
+
+# Initializing walking sequence
+'''The following represents the robot walking methodology.
+Option1: Shift weight to back legs and move both front legs.
+Option2: Shift weight to diagonal legs and move diagonals together.
+
+For either case, we used a 5th-order polynomial trajectory to make sure that
+all movement was smooth and even accounting for jerk etc.
+
+q(t) = A*(t**5) + B*(t**4) + C*(t**3) + D*(t**2) + E*t + F
+q'(t) = d/dt(q(t)) = 5*A*(t**4) + 4*B*(t**3) + 3*C*(t**2) + 2*D*t + E
+q''(t) = d/dt(q'(t)) = 20*A*(t**3) + 12*B*(t**2) + 6*C*(t) + 2*D
+
+with boundary conditions:
+
+At time t = 0; q(t) = 120, q'(t) = 0, q''(t) = 0
+At time t = T; q(T) = angle, q'(T) = 0, q''(t) = 0
+'''
+
+# t = 0
+# while True:
+#     servo1.move(sin(t) * 40 + 120)
+#     servo2.move(cos(t) * 40 + 120)
+#     servo3.move(sin(t) * 40 + 120)
+#     servo4.move(cos(t) * 40 + 120)
+#
+#     time.sleep(0.05)
+#     t += 0.1
