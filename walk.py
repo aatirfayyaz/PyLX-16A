ffc579fb42f15f14d8c33230e24cab16f2bb928b
Aatir Fayyaz <aatir.fayyaz@columbia.edu>
2022-11-14 19:50:38 -0500

    Troubleshooting on physical robot

--- walk.py
+++ walk.py
@@ -106,9 +106,9 @@ servo2Final = servo2Home + angleChange
 servo3Final = servo3Home - angleChange
 servo4Final = servo4Home + angleChange
 
-coeffA = 0.9877
-coeffB = 7.4074
-coeffC = 14.8148
+coeffA = 0.7407
+coeffB = 5.5556
+coeffC = 11.1111
 
 
 steps = 10
